7d26f4f330266ab45d98dcf4130486433f6ae8e6
Charles Feng <charles@charles.io>
2013-07-10 05:04:14 -0700

    minimal browser detection

--- js/hush.js
+++ js/hush.js
@@ -1,3 +1,8 @@
+if (typeof InstallTrigger !== 'undefined')
+  $('.firefox').show()
+else if (!!window.chrome && !(!!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0))
+  $('.chrome').show()
+
 $('.button-add').click(function () {
   chrome.webstore.install('https://chrome.google.com/webstore/detail/hjmoaenjknbdehbiaeeijcppnljflkff', function () {
     $('.button-add').prop('disabled', true)
