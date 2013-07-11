73d70e8c9052dc7d3308b6c6bcfaceb09a4d9f7e
Charles Feng <charles@charles.io>
2013-07-10 23:54:14 -0700

    platform icons everywhere!

--- js/global.js
+++ js/global.js
@@ -5,6 +5,18 @@ else if (!!window.chrome && !(!!window.opera || navigator.userAgent.indexOf(' OP
 else
   $('.browser').show()
 
+$('.toggle-chrome').click(function () {
+  $('.browser').hide()
+  $('.firefox').hide()
+  $('.chrome').show()
+})
+
+$('.toggle-firefox').click(function () {
+  $('.browser').hide()
+  $('.chrome').hide()
+  $('.firefox').show()
+})
+
 $('.chrome-add').click(function () {
   chrome.webstore.install('https://chrome.google.com/webstore/detail/hjmoaenjknbdehbiaeeijcppnljflkff', function () {
     $('.chrome-add').prop('disabled', true)
